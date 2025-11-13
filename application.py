45bf67649b7119df275f455cc83998d377babcaf
Sahana K N <sahana86gowda@gmail.com>
2025-11-13 19:19:08 +0530

    ready for render deployment

--- application.py
+++ application.py
@@ -39,4 +39,6 @@ def predict_datapoint():
         return render_template('home.html',results=round(results[0], 2))
     
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port = 5000)    
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
